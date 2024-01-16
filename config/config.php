c7d9ae69dc9ecdb7074373d7033c55e108fd9a40
IamJayGondaliya <prof.rnwjay2205@gmail.com>
2024-01-16 15:04:25 +0530

    Lec 2-1

--- config/config.php
+++ config/config.php
@@ -1,5 +1,6 @@
 <?php
 
+    //DB Helper
     class Config {
 
         //Attributes
@@ -7,17 +8,33 @@
         private $USERNAME = "root";
         private $PSW = "";
         private $DB_NAME = "php-3";
+        private $table_name = "student";
         public $conn;
 
         public function __construct() {
             $this->conn = mysqli_connect($this->HOST,$this->USERNAME,$this->PSW,$this->DB_NAME);
 
             if($this->conn) {
-                echo "Connected...";
             }
             else {
                 echo "Failled to connect...";
             }
+        }
+
+        //insert
+        public function insert($name,$age,$course) {
+
+            $query = "INSERT INTO $this->table_name(name,age,course) VALUES('$name',$age,'$course')";
+
+            $res = mysqli_query($this->conn,$query); //(object of mysqli_connect, query)   =>  1/0
+
+            if($res) {
+                echo "$res: Inserted successfully...";
+            }
+            else {
+                echo "Insertion failled...";
+            }
+
         }
 
     }
--- index.php
+++ index.php
@@ -4,24 +4,47 @@
 
     $config = new Config();
 
-    // //  mixly typed language.       =>  dart.
-    // //  staticly typed language     =>  fixed data types. c, c++
-    // //  dynamically typed language  =>  no predefined data types.   js, Php
+    /*
+        Associative array   =>  Map => key & value
 
-    // $a = 10;
-    // $b = 20;
-    // $sum = $a + $b;
-    // $data = "Sum of $a and $b: ";
+        Super globals:
 
-    // print "a: " . $a . "<br>";
-    // print "b: " . $b . "<br>";
-    // print $data . $sum;
-    // echo "<hr>";
+        $_GET       =>  Associative array   => by form method
+        $_POST      =>  Associative array   => by form method
+        $_REQUEST   =>  Associative array   => submit / button press
+        $_SERVER    =>  Associative array   => at the time of API call
 
-    // echo $a > $b ? "a is big" : "b is big";
+        Error handler:  '@'
+            - assigns value after initialization of the variable being assigned.
+    */
 
-    // echo "<hr>";
-    // echo "Hello PHP !! <br>" . "Another data <br>"; //  return nothing
-    // print "New DATA" . "Another new....";           //  returns 1
+    $submit_btn = @$_REQUEST['submit_btn'];
+
+    if(isset($submit_btn)) {
+        $name = $_POST['name'];
+        $age = $_POST['age'];
+        $course = $_POST['course'];
+
+        $config->insert($name,$age,$course);
+    }
 
 ?>
+
+<!-- Boiler plate   =>  !   : Emmet Abbreviation -->
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PHP Page</title>
+    </head>
+    <body>
+        <!-- get/post -->
+        <form action="" method="post">
+            Name: <input type="text" name="name"> <br>
+            Age: <input type="number" name="age"> <br>
+            Course: <input type="text" name="course"> <br>
+            <input type="submit" value="SUBMIT" name="submit_btn">
+        </form>
+    </body>
+</html>
